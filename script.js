39912b2f0ec7cd54487c85933550c2cdcdd81ae2
malakbahaa760-pixel <malakbahaa760@gmail.com>
2026-07-06 02:43:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 function addStar() {
   alert("الزر شغال ⭐");
-  function addStar() {
-  document.body.style.background = "red";
-  }
-}
-console.log("JS is working");
-
-function addStar() {
-  console.log("button clicked");
-}
+  
